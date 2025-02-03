b6cdbd8fb9a206f0b751b0e9370144bd6bd36367
Angie Tello <109442839+Glogie17@users.noreply.github.com>
2025-02-02 22:51:25 -0500

    Participacion.java

--- prj_00/src/controller/Participacion.java
+++ prj_00/src/controller/Participacion.java
@@ -6,16 +6,14 @@ public class Participacion {
         //Determinar nsi un alumno a aprobado o desaprobado el curso de java prog
         double nota = 16;
         
-        if (nota>=0 && nota<=20){
+        if (nota < 0 || nota > 20) {
+            System.out.println("CALIFICACIÓN INVÁLIDA");
+        } else if (nota < 10) {
             System.out.println("DESAPROBADO");
-            
-        } else if ((nota>10) && (nota<14)){
+        } else if (nota >= 10 && nota < 14) {
             System.out.println("PARA SUSTITUTORIO");
-        } else if (nota>=14){
+        } else { // nota >= 14 y dentro del rango válido
             System.out.println("APROBADO");
-            
-        }else{
-            System.out.println("CALIFICACION INVALIDA");
         }
         
     }
